00d19b213abb40bdec1a039c8b83215f198bb5a1
CandidDisk <marktajima@me.com>
2020-05-11 00:27:48 +0800

    Updates

---
+++ precache-manifest.2a67cc24c657aa3dc7ab0a97d6c5b142.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e00c8ef22153e8c83bdba2ab6e7b60",
+    "url": "/pm_mdr/index.html"
+  },
+  {
+    "revision": "71a9ae3e8385aaf8c395",
+    "url": "/pm_mdr/static/css/main.4da6db5c.chunk.css"
+  },
+  {
+    "revision": "fa75746aef5cb03cd89f",
+    "url": "/pm_mdr/static/js/2.1d7a9242.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/pm_mdr/static/js/2.1d7a9242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71a9ae3e8385aaf8c395",
+    "url": "/pm_mdr/static/js/main.d241d2f5.chunk.js"
+  },
+  {
+    "revision": "4e884105ef1140412033",
+    "url": "/pm_mdr/static/js/runtime-main.90fb98c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pm_mdr/static/media/logo.5d5d9eef.svg"
+  }
+]);
